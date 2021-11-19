cb788bfc77dcac5a5250ec751e5f49704922934e
Celso Cardoso <celso.pitta@gmail.com>
2021-11-19 00:08:13 -0300

    play

--- device_sample.py
+++ device_sample.py
@@ -84,7 +84,11 @@ if __name__ == "__main__":
         tokenizer = transformers.GPT2TokenizerFast.from_pretrained('gpt2')
 
         while True:
-            context = input("Type input:")
+            context = "<|endoftext|>context: The Normans (Norman: Nourmands; French: Normands; Latin: Normanni) were the people who in the" + \
+                    " 10th and 11th centuries gave their name to Normandy, a region in France. They were descended from Norse ('Norman' comes from 'Norseman') raiders and pirates from Denmark, Iceland and Norway who, under their leader Rollo, agreed to swear fealty to King Charles III of West Francia. Through generations of assimilation and mixing with the native Frankish and Roman-Gaulish populations, their descendants would gradually merge with the Carolingian-based cultures of West Francia. The distinct cultural and ethnic identity of the Normans emerged initially in the first half of the 10th century, and it continued to evolve over the succeeding centuries.\n" + \
+                    "\nquestion: In what country is Normandy located?" + \
+                    "\nanswer: "
+            #input("Type input:")
             tokens = tokenizer.encode(context)
 
             start = time.time()
